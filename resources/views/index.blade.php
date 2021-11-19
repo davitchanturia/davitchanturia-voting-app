f4c028810487b632adf578f598467f42f52a48c2
dato chanturia <datochanturia@redberry.ge>
2021-11-19 15:01:46 +0400

    style: markup for search ideas and head

---
+++ resources/views/index.blade.php
@@ -0,0 +1,47 @@
+<x-app-layout>
+    <div class="filters flex space-x-6 ">
+        <div class="w-1/3 ">
+
+            <select name="category" id="category" 
+                class="w-full rounded-xl px-4 py-2 border-none">
+
+                <option value="category one">category one</option>
+                <option value="category two">category two</option>
+                <option value="category three">category three</option>
+                <option value="category four">category four</option>
+            </select>
+
+        </div>
+
+        <div class="w-1/3">
+
+            <select name="other_filters" id="other_filters" 
+                class="w-full rounded-xl px-4 py-2 border-none">
+
+                <option value="category one">category one</option>
+                <option value="category two">category two</option>
+                <option value="category three">category three</option>
+                <option value="category four">category four</option>
+                
+            </select>
+
+        </div>
+
+        <div class="w-2/3 relative ">
+
+            <input type="search" placeholder="FInd an idea" 
+                class="w-full rounded-xl bg-white px-4 py-2 pl-10 border-none placeholder-gray-900"
+            >
+
+            <div class="absolute top-0 flex items-center h-full ml-2">
+                <svg class="w-6 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                </svg>
+            </div>
+
+        </div>
+        
+
+
+    </div>
+</x-app-layout>
