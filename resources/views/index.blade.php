abb95e7e719d8d5b52452df23af0f4d884e58de8
dato chanturia <datochanturia@redberry.ge>
2021-11-19 16:38:10 +0400

    style: markup for idea card

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -40,8 +40,80 @@
             </div>
 
         </div>
-        
-
 
     </div>
+
+
+    <div class="ideas-container space-y-6 my-6">
+        <div class="idea-container bg-white rounded-xl flex cursor-pointer
+            hover:shadow-card transition duration-150 ease-in
+            ">
+
+            {{-- first div --}}
+            <div class="border-r border-gray-100 px-5 py-8">
+
+                <div class="text-center">
+                    <div class="font-semibold text-2xl"> 12 </div>
+                    <div class="text-gray-500"> Votes </div>
+                </div>
+
+                <div class="mt-8">
+                    <button class="w-20 bg-gray-200 font-bold text-xxs uppercase rounded-xl px-4 py-3 border border-gray-200 
+                                 hover:border-gray-400 transition duration-150 ease-in">
+                        Vote
+                    </button>
+                </div>
+
+            </div>
+
+            {{-- second div --}}
+            <div class="flex px-2 py-6 ">
+                <a href="#" class="flex-none">
+                    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar" class="w-14 h-14 rounded-xl">
+                </a>
+                <div class="mx-4">
+                    <h4 class="text-xl font-semibold">
+                        <a href="#" class="hover:underline"> A random title can go here</a>
+                    </h4>
+
+                    <div class="text-gray-600 mt-3 line-clamp-3">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione accusamus omnis harum ipsum nulla non, repudiandae aspernatur doloribus aliquid maiores nisi quidem beatae amet! Suscipit vero soluta in explicabo asperiores voluptates odit, assumenda, dolores rem sequi animi libero natus iusto labore ipsum commodi laboriosam ad molestias eaque? Voluptas id laudantium iste fugiat natus ipsa fugit, est pariatur laborum? Nihil nulla porro soluta libero provident odit quia saepe tenetur a quasi corporis rem quae dolores, amet similique quas exercitationem ea? Maxime, necessitatibus, dolore nesciunt tempore unde asperiores ducimus quasi, ullam soluta repellat ratione. Architecto ad natus nam quas mollitia tenetur eveniet.
+                    </div>
+
+                    <div class="flex items-center justify-between mt-6">
+                        <div class="flex items-center text-xs font-semibold space-x-2 text-gray-400">
+                            <div>10 hours ago</div>
+                            <div>&bull;</div>
+                            <div>Category 1</div>
+                            <div>&bull;</div>
+                            <div class="text-gray-800">3 comments</div>
+                        </div>
+
+                        <div class="flex items-center space-x-2">
+                            <div class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">
+                                open
+                            </div>
+                            <button class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 py- px-3
+                                    transition duration-150 ease-in">
+                                <svg fill="currentColor" width="24" height="6"><path d="M2.97.061A2.969 
+                                    2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 
+                                    0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 
+                                    10-.003 5.94A2.97 2.97 0 0021.03.06z" style="color: rgba(163, 163, 163, .5)">
+                                </svg>
+
+                                <ul class="absolute w-44 text-left font-semibold text-sm bg-white shadow-dialog rounded-t-xl py-3 ml-8">
+                                    <li> <a href="#" class="hover:bg-gray-100 px-5 py-3 block">Mark as spam</a> </li>
+                                    <li> <a href="#" class="hover:bg-gray-100 px-5 py-3 block">Delete Post</a> </li>
+                                </ul>
+
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
 </x-app-layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -23,11 +23,18 @@ module.exports = {
                 70: '17.5rem',
                 175: '43.75rem',
             },
+            boxShadow: {
+                card: '4px 4px 15px 0 rgba(36, 37, 38, 0.08)',
+                dialog: '3px 4px 15px 0 rgba(36, 37, 38, 0.22)',
+            },
             maxWidth: {
                 custom: '62.5rem'
             },
             fontFamily: {
                 sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+            },
+            fontSize: {
+                xxs: ['0.625rem', { lineheight: '1rem'}],
             },
         },
     },
@@ -38,5 +45,8 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/line-clamp'),
+    ],
 };
