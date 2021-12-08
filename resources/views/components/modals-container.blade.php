1790c03fa301cc7679726481c90f20f51031a770
dato chanturia <datochanturia@redberry.ge>
2021-12-08 16:13:04 +0400

    refactor: refactoring modals
    modals markup is placed in one blade component according to the props

---
+++ resources/views/components/modals-container.blade.php
@@ -0,0 +1,7 @@
+<livewire:edit-idea :idea="$idea" />
+
+<livewire:delete-idea :idea="$idea" />
+
+<livewire:mark-idea-as-spam :idea="$idea" />
+
+<livewire:mark-idea-as-not-spam :idea="$idea" />
--- resources/views/idea/show.blade.php
+++ resources/views/idea/show.blade.php
@@ -14,13 +14,8 @@
         :votesCount="$votesCount" 
     />
 
-    <livewire:edit-idea :idea="$idea" />
-
-    <livewire:delete-idea :idea="$idea" />
-
-    <livewire:mark-idea-as-spam :idea="$idea" />
-
-    <livewire:mark-idea-as-not-spam :idea="$idea" />
+    <x-modals-container :idea="$idea"/>
+   
 
     <div class="comments-container space-y-6 md:ml-22 my-8 mt-1 pt-4 relative">
         <div class="comment-container relative flex flex-1 px-4 py-6 ">
